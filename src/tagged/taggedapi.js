e077aa8489cf15c4a6372a671a557334b073ed68
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2022-01-17 13:57:27 +0530

    v
    v

---
+++ src/tagged/taggedapi.js
@@ -0,0 +1,33 @@
+const taggedapi = [
+    {
+        id= 1,
+        imgtagged: img1,
+        text: 'Hii this is dummy text card one'
+    },
+
+    {
+        id= 2,
+        imgtagged: img2,
+        text: 'Hii this is dummy text card two'
+    },
+
+    {
+        id= 3,
+        imgtagged: img3,
+        text: 'Hii this is dummy text card three'
+    },
+
+    {
+        id= 4,
+        imgtagged: img4,
+        text: 'Hii this is dummy text card four'
+    },
+
+    {
+        id= 5,
+        imgtagged: img5,
+        text: 'Hii this is dummy text card five'
+    }
+]
+
+export default taggedapi;
